bdbc982023e51c4aa9357c2643a30f27e627e04e
YanickDal <yan.dal@hotmail.com>
2023-11-24 16:52:05 -0500

    Created dependencies injections

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,10 +1,32 @@
 import 'package:get_it/get_it.dart';
+import 'package:ubee_mini/features/authentication/data/data_source/firebase_user_api.dart';
+import 'package:ubee_mini/features/authentication/data/data_source/user_api.dart';
+import 'package:ubee_mini/features/authentication/data/repository/user_repository_impl.dart';
+import 'package:ubee_mini/features/authentication/domain/repository/user_repository.dart';
+import 'package:ubee_mini/features/authentication/domain/use_cases/create_user.dart';
+import 'package:ubee_mini/features/authentication/domain/use_cases/email_validation.dart';
+import 'package:ubee_mini/features/authentication/domain/use_cases/password_match_check.dart';
+import 'package:ubee_mini/features/authentication/domain/use_cases/password_validation.dart';
+import 'package:ubee_mini/features/authentication/presentation/bloc/authentication_bloc.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  //! Feature -
+  //! Feature - Authentication
+  // Bloc
+  sl.registerFactory<AuthenticationBloc>(()=> AuthenticationBloc());
+  // Repository
+  sl.registerLazySingleton<UserApi>(() => FireBaseUserApi());
+  sl.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(sl()));
+  // DataSource
 
+  // Usecases
+  sl.registerLazySingleton<CreateUser>(() => CreateUser(sl()));
+  sl.registerLazySingleton<EmailValidation>(() => EmailValidation());
+  sl.registerLazySingleton<PasswordMatchCheck>(() => PasswordMatchCheck());
+  sl.registerLazySingleton<PasswordValidation>(() => PasswordValidation());
+
+    //! Feature -
   // Bloc
 
   // Repository
@@ -12,4 +34,5 @@ Future<void> init() async {
   // DataSource
 
   // Usecases
+
 }
